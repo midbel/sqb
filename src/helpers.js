2a68ae29d03cdf3fda9f7db23edff19cc73876d9
Nicolas Brun <nbrun@gld.be>
2024-04-19 19:51:26 +0200

    remove build js files

--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toStr = void 0;
-const commons_1 = require("./commons");
-function toStr(item) {
-	return (0, commons_1.isSql)(item) ? item.sql() : item;
-}
-exports.toStr = toStr;
