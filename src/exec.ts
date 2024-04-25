2a6f2b164d1677a6e1c6e5bd4f35df6b37350c21
Nicolas Brun <nbrun@gld.be>
2024-04-25 21:39:23 +0200

    check number of arguments given to function

--- src/exec.ts
+++ src/exec.ts
@@ -3,46 +3,79 @@ import { toStr } from "./helpers";
 
 export class Exec implements Sql {
 	static sum(args: Array<SqlElement>): Sql {
+		if (args.length !== 1) {
+			throw new Error("sum: expected 1 argument");
+		}
 		return new Exec("sum", args);
 	}
 
 	static len(args: Array<SqlElement>): Sql {
+		if (args.length !== 1) {
+			throw new Error("length: expected 1 argument");
+		}
 		return new Exec("length", args);
 	}
 
 	static mod(args: Array<SqlElement>): Sql {
+		if (args.length !== 2) {
+			throw new Error("mod: expected 2 arguments");
+		}
 		return new Exec("mod", args);
 	}
 
 	static substr(args: Array<SqlElement>): Sql {
+		if (args.length !== 3) {
+			throw new Error("substr: expected 3 arguments");
+		}
 		return new Exec("substr", args);
 	}
 
 	static coalesce(args: Array<SqlElement>): Sql {
+		if (args.length === 0) {
+			throw new Error("coalesce: expected at least 1 argument");
+		}
 		return new Exec("coalesce", args);
 	}
 
 	static ifnull(args: Array<SqlElement>): Sql {
+		if (args.length !== 2) {
+			throw new Error("ifnull: expected 2 arguments");
+		}
 		return new Exec("ifnull", args);
 	}
 
 	static isnull(args: Array<SqlElement>): Sql {
+		if (args.length !== 2) {
+			throw new Error("isnull: expected 2 arguments");
+		}
 		return new Exec("isnull", args);
 	}
 
 	static avg(args: Array<SqlElement>): Sql {
+		if (args.length !== 0) {
+			throw new Error("avg: expected at least 1 argumen");
+		}
 		return new Exec("avg", args);
 	}
 
 	static count(args: Array<SqlElement>): Sql {
+		if (args.length !== 1) {
+			throw new Error("count: expected 1 argument");
+		}
 		return new Exec("count", args);
 	}
 
 	static min(args: Array<SqlElement>): Sql {
+		if (args.length !== 1) {
+			throw new Error("min: expected 1 argument");
+		}
 		return new Exec("min", args);
 	}
 
 	static max(args: Array<SqlElement>): Sql {
+		if (args.length !== 1) {
+			throw new Error("max: expected 1 argument");
+		}
 		return new Exec("max", args);
 	}
 
